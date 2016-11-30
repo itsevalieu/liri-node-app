14e6c4194a915ab0e8a576d0cd71d5b11dd3d282
Eva Lieu <itsevalieu@gmail.com>
2016-11-30 12:57:06 -0800

    Added consumer and access keys to the keys.js file

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'MpAvbfPnryLWWsVvbb7iCFvrv',
+  consumer_secret: '7HKjQVTcd6x9PicoTZkwBAmIljusd4yqp1CtA2nPqCYsLmjMvX',
+  access_token_key: '4769508458-DHhp2PS3i8hrUowqEBppI5HpDvlfKrlLr5cEUCX',
+  access_token_secret: '8eIGhiA12GgYBICKHvms4M9weReHrycuVYi4C6Zml24OC',
 }
