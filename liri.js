c5f70e8c745756505c131f9a113fe990700046b7
Eva Lieu <itsevalieu@gmail.com>
2016-12-04 15:38:06 -0800

    Complete callback for movie function.

--- liri.js
+++ liri.js
@@ -9,17 +9,6 @@ var client = new Twitter({
     access_token_secret: keys.twitterKeys.access_token_secret,
 });
 
-var spotify = require('spotify'); 
-var song = "dancing in the moonlight";
-spotify.search({ type: 'track', query: song }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    // Do something with 'data' 
-    console.log(data);
-});
-
 /*
 switch(action) {
 	case "my-tweets":
@@ -41,7 +30,7 @@ switch(action) {
 */
 function myTweets(){
 	//* This will show your last 20 tweets and when they were created at in your terminal/bash window.
-	var params = {screen_name: "itsevalieu", count: 3};
+	var params = {screen_name: "itsevalieu", count: 20};
 
     client.get("statuses/user_timeline", params, function(error, tweet, response) {
         if (!error) {
@@ -55,6 +44,7 @@ function myTweets(){
 }
 
 function spotifyThisSong(){
+
 /* 
 *This will show the following information about the song in your terminal/bash window
     * Artist(s)
@@ -64,31 +54,55 @@ function spotifyThisSong(){
 
 * if no song is provided then your program will default to
     * "The Sign" by Ace of Base
+
+var spotify = require('spotify'); 
+
+var song = "dancing in the moonlight";
+spotify.search({ type: 'track', query: song }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    // Do something with 'data' 
+    console.log(data);
+});
+spotify.search({ type: 'artist', query: song }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    // Do something with 'data' 
+    console.log(data);
+});
 */
 }
 
+
 function movieThis(){
-/*
-* This will output the following information to your terminal/bash window:
-
-    * Title of the movie.
-    * Year the movie came out.
-    * IMDB Rating of the movie.
-    * Country where the movie was produced.
-    * Language of the movie.
-    * Plot of the movie.
-    * Actors in the movie.
-    * Rotten Tomatoes Rating.
-    * Rotten Tomatoes URL.
-
-* If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    * If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-    * It's on Netflix!
-
-*/
+    var request = require('request');
+    var movieTitle = "Mr+Nobody";
+    request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var str = JSON.parse(body);
+            console.log("Title: " + str.Title);
+            console.log("Year: " + str.Year);
+            console.log("IMDB Rated: " + str.Rated);
+            console.log("Country: " + str.Country);
+            console.log("Language: " + str.Language);
+            console.log("Plot: " + str.Plot);
+            console.log("Actors: " + str.Actors);
+            console.log("Rotten Tomatoes Rating: " + str.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + str.tomatoURL);
+        }
+    });
 }
 function doWhatItSays(){
 /*
+Var fs = require ('fs`);
+fs.readFile('/etc/passwd', (err, data) => {
+if (err) throw err;
+console.log(data);
+});
 Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 Feel free to change the text in that document to test out the feature for other commands.
