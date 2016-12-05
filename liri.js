ab943ebbcce4fa90d629b7c8d877850b53b6edf5
Eva Lieu <itsevalieu@gmail.com>
2016-12-04 16:12:58 -0800

    Uncomment switch statement. Twitter and movie function fully functional, may want to work on parameter for movie function, commence work on spotify function

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 var keys = require("./keys.js");
 var action = process.argv[2];
+var movie = process.argv[3];
 
-var Twitter = require('twitter');
+var Twitter = require("twitter");
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
@@ -9,7 +10,6 @@ var client = new Twitter({
     access_token_secret: keys.twitterKeys.access_token_secret,
 });
 
-/*
 switch(action) {
 	case "my-tweets":
 	myTweets();
@@ -20,25 +20,23 @@ switch(action) {
 	break;
 
 	case "movie-this":
-	movieThis();
+	movieThis(movie);
 	break;
 
 	case "do-what-it-says":
 	doWhatItSays();
 	break;
 }
-*/
+
 function myTweets(){
 	//* This will show your last 20 tweets and when they were created at in your terminal/bash window.
 	var params = {screen_name: "itsevalieu", count: 20};
-
     client.get("statuses/user_timeline", params, function(error, tweet, response) {
         if (!error) {
-            console.log(tweet);
-            console.log(tweet[0].text);
-            console.log(tweet[0].created_at);
-            console.log(tweet[1].text);
-            console.log(tweet[1].created_at);
+            for(var i = 0; i < 20; i++){
+                console.log("====================================================");
+                console.log((i+1)+ ". " + "Created at: " + tweet[i].created_at + "\nTweet: " + tweet[i].text + "\n");
+            }
         }
     });
 }
@@ -78,10 +76,13 @@ spotify.search({ type: 'artist', query: song }, function(err, data) {
 }
 
 
-function movieThis(){
-    var request = require('request');
-    var movieTitle = "Mr+Nobody";
-    request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+function movieThis(movie){
+    var request = require("request");
+    var title = movie;
+    if(title === undefined){
+        title = "mr+nobody";
+    }
+    request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var str = JSON.parse(body);
             console.log("Title: " + str.Title);
@@ -96,13 +97,19 @@ function movieThis(){
         }
     });
 }
+
+
 function doWhatItSays(){
+    var fs = require ("fs");
+    
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if(error){
+            return console.log("Error! Something went wrong!");
+        }
+        console.log(data);
+        spotifyThisSong();//send in parameter for song
+    });
 /*
-Var fs = require ('fs`);
-fs.readFile('/etc/passwd', (err, data) => {
-if (err) throw err;
-console.log(data);
-});
 Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 Feel free to change the text in that document to test out the feature for other commands.
