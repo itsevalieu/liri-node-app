eeb83b03ec52953ce5efb5e48fb3186a12c11762
Eva Lieu <itsevalieu@gmail.com>
2017-03-18 10:53:18 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'MpAvbfPnryLWWsVvbb7iCFvrv',
-  consumer_secret: '7HKjQVTcd6x9PicoTZkwBAmIljusd4yqp1CtA2nPqCYsLmjMvX',
-  access_token_key: '4769508458-DHhp2PS3i8hrUowqEBppI5HpDvlfKrlLr5cEUCX',
-  access_token_secret: '8eIGhiA12GgYBICKHvms4M9weReHrycuVYi4C6Zml24OC',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 }
